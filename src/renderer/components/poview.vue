816632838aabe496abd51b7d5ab8b357e5d2a71c
Libo Li <liboli.lilibo@gmail.com>
2018-07-26 22:46:02 -0400

    not sure what's happening but it stopped throwing errors... so we're off to a good start i guess

--- src/renderer/components/poview.vue
+++ src/renderer/components/poview.vue
@@ -1,14 +1,31 @@
 <template>
   <div>
-    hello
+	<div class="potree_container" style="position: absolute; width: 100%; height: 100%; left: 0px; top: 0px; ">
+		<div id="potree_render_area"></div>
+		<div id="potree_sidebar_container"> </div>
+	</div>
   </div> 
 </template>
 
 <script>
-import Potree from '../potree/potree';
+/* eslint-disable */
+import Potree from '../potree/potree'
 
 export default {
   name: 'poview',
+  data(){
+    return {
+      viewer: null,
+    }
+  },
+  mounted(){
+    this.initViewer();
+  },
+  methods:{
+    initViewer(){
+      this.viewer = new Potree.Viewer(document.getElementById('potree_render_area'));
+    }
+  }
 };
 </script>
 
